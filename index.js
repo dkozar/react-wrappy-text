cc65bb4bd42df178a50ff0483322a344b49cb070
Danko Kozar <github@dankokozar.com>
2016-01-14 00:17:24 +0000

    Added module.exports

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports = require('./src/components/WrappyText');
+module.exports.TextRotator = require('./src/components/TextRotator');
+module.exports.ScrollListener = require('./src/components/ScrollListener');
